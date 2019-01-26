eb82644d5a4d71b53cf6d509aaebc7764ae5e2e8
nielsjani <NielsJani>
2019-01-26 17:52:38 +0100

    change gitignore

--- out/production/resources/db/migration/V2__INSERT_DATA_INTO_SITARGET.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO SEPA_RATE.sql_injection_target values ('ID1', 'MYSQL');
-INSERT INTO SEPA_RATE.sql_injection_target values ('ID2', 'ORACLESQL');
-INSERT INTO SEPA_RATE.sql_injection_target values ('ID3', 'POSTGRESQL');
-INSERT INTO SEPA_RATE.sql_injection_target values ('ID4', 'NOSQL');
